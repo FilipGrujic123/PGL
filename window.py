d3838c4fa59201206eec323c07a870f3806964ef
FilipGrujic123 <wulfobusinessreal@gmail.com>
2022-06-24 14:12:33 +0200

    added camera offset

--- window.py
+++ window.py
@@ -12,27 +12,37 @@ def draw_regular_polygon(surface, color, vertex_count, radius, position, width=0
     ], width)
 
 class Window:
+    cameraOffset = [0, 0]
+
     def __init__(self, dimensions, title, icon=pygame.image.load(f'{os.path.dirname(os.path.realpath(__file__))}/icon.png'), flags=0, depth=0, display=0, vsync=0):
         self.win = pygame.display.set_mode(dimensions, flags, depth, display, vsync)
         pygame.display.set_caption(title)
         pygame.display.set_icon(icon)
+
+    @abstractclassmethod
+    def getCameraOffset():
+        return Window.cameraOffset
+    
+    @abstractclassmethod
+    def setCameraOffset(offset):
+        Window.cameraOffset = offset
         
     def draw(self, entity):
         type = entity.type
 
         if type == LINE_TYPE:
-            pygame.draw.line(self.win, entity.color, entity.pos, (entity.pos[0] + entity.scale[0], entity.pos[1] + entity.scale[1]), entity.thickness)
+            pygame.draw.line(self.win, entity.color, (entity.pos[0] - Window.getCameraOffset[0], entity.pos[1] - Window.getCameraOffset[1]), (entity.pos[0] - Window.getCameraOffset[0] + entity.scale[0], entity.pos[1] - Window.getCameraOffset[1] + entity.scale[1]), entity.thickness)
         if type == CIRCLE_TYPE:
-            pygame.draw.circle(self.win, entity.color, entity.pos, entity.scale, entity.thickness)
+            pygame.draw.circle(self.win, entity.color, (entity.pos[0] - Window.getCameraOffset[0], entity.pos[1] - Window.getCameraOffset[1]), entity.scale, entity.thickness)
         if type == SQUARE_TYPE:
-            pygame.draw.rect(self.win, entity.color, pygame.Rect(entity.pos, entity.scale), entity.thickness)
+            pygame.draw.rect(self.win, entity.color, pygame.Rect(entity.pos[0] - Window.getCameraOffset[0], entity.pos[1] - Window.getCameraOffset[1], entity.scale[0], entity.scale[1]), entity.thickness)
         if type == PENTAGON_TYPE:
-            draw_regular_polygon(self.win, entity.color, 5, entity.scale, entity.pos, entity.thickness)
+            draw_regular_polygon(self.win, entity.color, 5, entity.scale, (entity.pos[0] - Window.getCameraOffset[0], entity.pos[1] - Window.getCameraOffset[1]), entity.thickness)
         if type == HEXAGON_TYPE:
-            draw_regular_polygon(self.win, entity.color, 6, entity.scale, entity.pos, entity.thickness)
+            draw_regular_polygon(self.win, entity.color, 6, entity.scale, (entity.pos[0] - Window.getCameraOffset[0], entity.pos[1] - Window.getCameraOffset[1]), entity.thickness)
         if type == IMAGE_TYPE:
             rotated_image = pygame.image.flip(pygame.transform.rotate(pygame.transform.scale(entity.sprite, entity.scale), entity.rot), (entity.rotX, entity.rotY))
-            new_rect = rotated_image.get_rect(center = entity.sprite.get_rect(center = (entity.pos[0], entity.pos[1])).center)
+            new_rect = rotated_image.get_rect(center = entity.sprite.get_rect(center = (entity.pos[0] - Window.getCameraOffset[0], entity.pos[1] - Window.getCameraOffset[1])).center)
             self.win.blit(rotated_image, new_rect)
         
     def fill(self, color):
